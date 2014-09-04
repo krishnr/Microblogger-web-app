6f6445fe3a60580b9853bd5f4fbe03a311d79cd1
Krishn Ramesh <krishnr@gmail.com>
2014-09-04 14:33:51 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,45 +1,38 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+subject{page}
+
   describe "Home page" do
+  	before {visit root_path}
 
-    it "should have the content 'Microblogger'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Microblogger')
-    end
+    it {should have_content('μblogr')}
+    it {should have_title (full_title(''))}
+    it {should_not have_title('| Home')}
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Microblogger")
-    end
-
-    it "should not have a custom page title" do
-    	visit '/static_pages/home'
-    	expect(page).not_to have_title('| Home')
-    end
   end
 
   describe "Help page" do
-  	it "should have the content 'Help'" do
-		visit '/static_pages/help'
-      	expect(page).to have_content('Help')
-    end
+  	before {visit help_path}
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Microblogger | Help")
-    end
+  	it {should have_content('Help')}
+    it {should have_title(full_title('Help'))}
+    
   end
 
   describe "About page" do
-  	it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+  	before {visit about_path}
 
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Microblogger | About Us")
-    end
+  	it {should have_content('About Us')}
+    it {should have_title(full_title('About Us'))}
+  end
+
+  describe "Contact page" do
+  	before{visit contact_path}
+
+    it {should have_content('Contact')}
+
+    it {should have_title(full_title('Contact'))}
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,11 +1,8 @@
-module ApplicationHelper
-	# Returns the full title on a per-page basis.
-  def full_title(page_title)
+def full_title(page_title)
     base_title = "μblogr"
     if page_title.empty?
       base_title
     else
       "#{base_title} | #{page_title}"
     end
-  end
 end
